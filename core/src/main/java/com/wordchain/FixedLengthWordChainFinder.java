87887cdf410eab99bed2ea913f9d60b51678f5e6
xDzmShe <xdzmshe@gmail.com>
2018-07-30 13:16:05 +0300

    Added stub for WordChainFinder

---
+++ core/src/main/java/com/wordchain/FixedLengthWordChainFinder.java
@@ -0,0 +1,14 @@
+package com.wordchain;
+
+import java.util.List;
+
+public class FixedLengthWordChainFinder implements WordChainFinder {
+
+	private final String PATH_TO_DICTIONARY = "wordlist.txt";
+
+	public List<String> findChain(final String startWord, final String endWord) {
+		// loadDictionary
+		// findPath
+		return null;
+	}
+}
--- core/src/main/java/com/wordchain/WordChainApplication.java
+++ core/src/main/java/com/wordchain/WordChainApplication.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class WordChainApplication {
 
-	private static WordChainFinder wordChainFinder;
+	private static WordChainFinder wordChainFinder = new FixedLengthWordChainFinder();
 	
 	public static void main(String[] args) {
 
@@ -13,8 +13,8 @@ public class WordChainApplication {
 			System.exit(1);
 		}
 
-		//List<String> wordChain = wordChainFinder.findChain(args[0], args[1]);
-		//System.out.println(wordChain != null ? wordChain : "Cannot find solution");
+		List<String> wordChain = wordChainFinder.findChain(args[0], args[1]);
+		System.out.println(wordChain != null ? wordChain : "Cannot find solution");
 	}
 
 	static boolean validateCommandLineArguments(String[] args) {
