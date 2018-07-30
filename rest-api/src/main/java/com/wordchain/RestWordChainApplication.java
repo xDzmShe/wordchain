9cbb93ecbb432985fb6e5195fa17d46346d78264
xDzmShe <xdzmshe@gmail.com>
2018-07-30 18:52:51 +0300

    Added Spring Boot REST API

---
+++ rest-api/src/main/java/com/wordchain/RestWordChainApplication.java
@@ -0,0 +1,11 @@
+package com.wordchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWordChainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestWordChainApplication.class, args);
+	}
+}
---
+++ rest-api/src/main/java/com/wordchain/controller/WordChainController.java
@@ -0,0 +1,27 @@
+package com.wordchain.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordchain.FixedLengthWordChainFinder;
+import com.wordchain.WordChainFinder;
+
+@RestController
+@RequestMapping("/wordchain")
+public class WordChainController {
+
+	@GetMapping
+	public List<String> findWordChain(@RequestParam(value = "start_word", required = true) String startWord,
+			@RequestParam(value = "end_word", required = true) String endWord) {
+
+		List<String> wordChain = null;
+		WordChainFinder wordChainPuzzle = new FixedLengthWordChainFinder();
+		wordChain = wordChainPuzzle.findChain(startWord, endWord);
+
+		return wordChain;
+	}
+}
