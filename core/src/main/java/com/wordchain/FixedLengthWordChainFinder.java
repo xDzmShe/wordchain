39a20451496914736df953486f2856408e5a1488
xDzmShe <xdzmshe@gmail.com>
2018-07-30 17:58:37 +0300

    Added loop to find word chain

--- core/src/main/java/com/wordchain/FixedLengthWordChainFinder.java
+++ core/src/main/java/com/wordchain/FixedLengthWordChainFinder.java
@@ -4,24 +4,30 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FixedLengthWordChainFinder implements WordChainFinder {
 
+	public FixedLengthWordChainFinder() {
+		super();
+	}
+
 	private final String PATH_TO_DICTIONARY = "wordlist.txt";
 
 	/**
-	 * Find word chain from startWord to endWord
+	 * Finds word chain from startWord to endWord
 	 * 
 	 * @param startWord
 	 *            First word in the chain
 	 * @param endWord
 	 *            Last word in the chain
-	 * 
 	 */
 	public List<String> findChain(final String startWord, final String endWord) {
 		if (startWord.equals(endWord)) {
@@ -29,11 +35,35 @@ public class FixedLengthWordChainFinder implements WordChainFinder {
 			return null;
 		}
 
+		List<String> dictionary = loadDictionary(startWord.length());
+
 		Map<String, String> listOfPrevious = new HashMap<>();
 		boolean isPathFound = false;
 
+		Queue<String> queue = new LinkedList<String>();
+		queue.add(startWord);
+
 		// build tree of words that different from each other only by one character
 		// store path from bottom of the tree to the root in listOfPrevious
+		while (!queue.isEmpty() && isPathFound == false) {
+			String currentWord = queue.remove();
+			List<String> listOfNextWords = getNextWords(currentWord, dictionary);
+
+			for (String word : listOfNextWords) {
+
+				if (word.equals(endWord)) {
+					listOfPrevious.put(word, currentWord);
+					isPathFound = true;
+					break;
+				}
+
+				if (!listOfPrevious.containsKey(word)) {
+					listOfPrevious.put(word, currentWord);
+					queue.add(word);
+				}
+			}
+
+		}
 
 		if (isPathFound) {
 			return getPath(listOfPrevious, startWord, endWord);
@@ -43,7 +73,7 @@ public class FixedLengthWordChainFinder implements WordChainFinder {
 	}
 
 	/**
-	 * Loads words from file
+	 * Loads words with length wordLength from file
 	 * 
 	 * @param wordLength
 	 *            Length of words that should be load from the dictionary
@@ -60,11 +90,46 @@ public class FixedLengthWordChainFinder implements WordChainFinder {
 			e.printStackTrace();
 		}
 		return wordList;
-
 	}
 
 	/**
-	 * Get path from startWord to endWord based on listOfPrevious
+	 * Finds list of words that differs from the given word by one character
+	 * 
+	 * @param word
+	 *            Word to compare
+	 * @param dictionary
+	 *            Dictionary where to find words that differ by one character
+	 */
+	List<String> getNextWords(final String word, List<String> dictionary) {
+		List<String> nextWords = new ArrayList<>();
+		for (String currWord : dictionary) {
+			if (getDiff(word, currWord) == 1) {
+				nextWords.add(currWord);
+			}
+		}
+		return nextWords;
+	}
+
+	/**
+	 * Compares two words and returns how many differences between them
+	 * 
+	 * @param word1
+	 *            First word to compare
+	 * @param word2
+	 *            Second word to compare
+	 */
+	int getDiff(final String word1, final String word2) {
+		int diff = 0;
+		for (int i = 0; i < word1.length(); i++) {
+			if (word1.charAt(i) != word2.charAt(i)) {
+				diff++;
+			}
+		}
+		return diff;
+	}
+
+	/**
+	 * Creates path from startWord to endWord based on listOfPrevious
 	 * 
 	 * @param listOfPrevious
 	 *            Contains map with word pairs from bottom of the tree to the root
@@ -72,9 +137,16 @@ public class FixedLengthWordChainFinder implements WordChainFinder {
 	 *            First word in the chain
 	 * @param endWord
 	 *            Last word in the chain
-	 * 
 	 */
 	List<String> getPath(final Map<String, String> listOfPrevious, final String startWord, final String endWord) {
-		return null;
+		List<String> path = new LinkedList<>();
+		String word = endWord;
+		while (!word.equals(startWord)) {
+			path.add(0, word);
+			word = listOfPrevious.get(word);
+		}
+
+		path.add(0, startWord);
+		return path;
 	}
 }
--- core/src/test/java/com/wordchain/FixedLengthWordChainFinderTest.java
+++ core/src/test/java/com/wordchain/FixedLengthWordChainFinderTest.java
@@ -1,21 +1,23 @@
 package com.wordchain;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 
 public class FixedLengthWordChainFinderTest {
 
-	private final List<String> DICTIONARY = Arrays.asList("1111", "1112", "111", "112", "121", "211", "221", "222",
-			"223", "232", "322", "332", "333", "334", "343", "433", "443", "444");
+	private final List<String> DICTIONARY = Arrays.asList("111", "112", "121", "211", "221", "222", "223", "232", "322",
+			"332", "333", "334", "343", "433", "443", "444");
 
 	private final int WORD_LENGTH = 3;
 
@@ -30,35 +32,49 @@ public class FixedLengthWordChainFinderTest {
 		LIST_OF_PREVIOUS.put("222", "221");
 	}
 
-	class WordChainMock extends FixedLengthWordChainFinder {
-	}
-
-	@Mock
-	WordChainMock wordChainMock = mock(WordChainMock.class);
+	@Spy
+	FixedLengthWordChainFinder wordChainMock = spy(FixedLengthWordChainFinder.class);
 
 	@Test
 	public void testFindChain_SameStartAndEndWords() {
-		when(wordChainMock.loadDictionary(WORD_LENGTH)).thenReturn(DICTIONARY);
-		when(wordChainMock.findChain("111", "111")).thenCallRealMethod();
+		Mockito.doReturn(DICTIONARY).when(wordChainMock).loadDictionary(WORD_LENGTH);
 		assertEquals(wordChainMock.findChain("111", "111"), null);
 	}
 
 	@Test
-	public void testFindChain_Successful_SortChain() {
+	public void testFindChain_Successful_ShortChain() {
 		when(wordChainMock.loadDictionary(WORD_LENGTH)).thenReturn(DICTIONARY);
-		when(wordChainMock.findChain("111", "222")).thenCallRealMethod();
 		assertEquals(wordChainMock.findChain("111", "222"), Arrays.asList("111", "121", "221", "222"));
 	}
 
 	@Test
 	public void testFindChain_Unsuccessful() {
 		when(wordChainMock.loadDictionary(WORD_LENGTH)).thenReturn(DICTIONARY);
-		when(wordChainMock.findChain("111", "777")).thenCallRealMethod();
 		assertEquals(wordChainMock.findChain("111", "777"), null);
 	}
 
 	@Test
 	public void testGetPath() {
 		assertEquals(wordChainMock.getPath(LIST_OF_PREVIOUS, "111", "222"), Arrays.asList("111", "121", "221", "222"));
+	}
+
+	@Test
+	public void testGetDiff_OneDifference() {
+		assertEquals(wordChainMock.getDiff("111", "112"), 1);
+	}
+
+	@Test
+	public void testGetDiff_TwoDifferences() {
+		assertEquals(wordChainMock.getDiff("111", "212"), 2);
+	}
+
+	@Test
+	public void testGetNextWords_wordsFound() {
+		assertEquals(wordChainMock.getNextWords("111", DICTIONARY), Arrays.asList("112", "121", "211"));
+	}
+
+	@Test
+	public void testGetNextWords_wordsNotFound() {
+		assertEquals(wordChainMock.getNextWords("777", DICTIONARY), new ArrayList<String>());
 	}
 }
