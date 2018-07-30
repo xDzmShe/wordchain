b33970ca7396cb613f509c9c509fe44285453ce8
xDzmShe <xdzmshe@gmail.com>
2018-07-30 11:19:23 +0300

    Added main method

---
+++ core/src/main/java/com/wordchain/WordChainApplication.java
@@ -0,0 +1,19 @@
+package com.wordchain;
+
+public class WordChainApplication {
+
+	public static void main(String[] args) {
+
+		// validate command line arguments
+		if (!validateCommandLineArguments(args)) {
+			System.exit(1);
+		}
+
+		// find word chain
+		// print result
+	}
+
+	static boolean validateCommandLineArguments(String[] args) {
+		return false;
+	}
+}
